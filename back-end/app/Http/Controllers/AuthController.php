159e66fd0c0facbb07448d923fc499c28a5347f8
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-11 22:49:06 +0330

    feat(auth/controller): Add and handle Auth controller

---
+++ back-end/app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'username' => $request->username,
+            'location' => $request->location,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'username', 'password');
+
+        if (isset($credentials['email'])) {
+            $user = User::where('email', $credentials['email'])->first();
+        } else {
+            $user = User::where('username', $credentials['username'])->first();
+        }
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    }
+}
