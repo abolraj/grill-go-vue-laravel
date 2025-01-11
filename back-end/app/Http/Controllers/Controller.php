f9c1104510e24a6cd062ee53e35856f9dad2091b
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-11 21:53:05 +0330

    feat(Swagger): Add and handle swagger API documentation with L5Swagger

--- back-end/app/Http/Controllers/Controller.php
+++ back-end/app/Http/Controllers/Controller.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Grill Go | API Documentation",
+ *      description="You can look for api endpoints and their usage."
+ * )
+ *
+ */
+
 abstract class Controller
 {
     //
