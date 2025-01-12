7ed50942751614ff320b4f0f98dc874470d89b97
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-12 13:38:31 +0330

    feat(routes/api): Add and handle foods, orders, users, and logout api routes

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('users', UserController::class)
+        ->except(['store']);
+    Route::apiResource('foods', FoodController::class);
+    Route::apiResource('orders', OrderController::class);
 });
