177fa30c74fcee16aa58d9ebc6f3b29eada1bd7d
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-12 13:29:21 +0330

    feat(foods_table): Add foods table migration

---
+++ back-end/database/migrations/2025_01_12_060814_create_foods_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('price');
+            $table->integer('number');
+            $table->foreignId('parent_id')->nullable()->constrained('foods', 'id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('food');
+    }
+};
