8647388d2d72a0db2225d3785d4882dfa3c023ba
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-26 13:57:25 +0330

    feat - Add and handle orders_foods pivot table for orders and foods

---
+++ back-end/database/migrations/2025_01_25_062021_create_orders_foods_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders_foods', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->foreignId('food_id')->constrained('foods', 'id')->cascadeOnDelete();
+            $table->foreignId('order_id')->constrained('orders', 'id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders_foods');
+    }
+};
