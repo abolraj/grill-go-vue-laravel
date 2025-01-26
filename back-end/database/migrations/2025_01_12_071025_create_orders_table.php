415cde77ef81d81751d3d719f029e0527a51c651
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-26 13:59:32 +0330

    feat(migration): Add category, note field to foods table

--- back-end/database/migrations/2025_01_12_071025_create_orders_table.php
+++ back-end/database/migrations/2025_01_12_071025_create_orders_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->integer('total_number');
-            $table->unsignedBigInteger('total_price');
+            $table->text('note')->nullable();
             $table->smallInteger('status');
             $table->integer('duration');
             $table->foreignId('customer_id')->constrained('users', 'id')->cascadeOnDelete();
-            $table->foreignId('food_id')->constrained('foods', 'id')->cascadeOnDelete();
             $table->timestamps();
         });
     }
